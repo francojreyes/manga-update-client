8f1bf7d4b0655f4b19ab248408e63d2e3bf9f676
Franco Reyes <franco.javier.reyes@gmail.com>
2024-08-13 02:53:57 +1000

    Sign in with Discord

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,24 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
+  },
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "/instance/0/manga",
+        permanent: true,
+      },
+    ];
   },
 };
 
